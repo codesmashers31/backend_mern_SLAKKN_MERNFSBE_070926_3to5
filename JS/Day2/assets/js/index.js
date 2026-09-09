42c63e97727e842ff783a3c95991e6661731a774
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-09 18:08:34 +0530

    day2

---
+++ JS/Day2/assets/js/index.js
@@ -0,0 +1,134 @@
+
+
+// var    - 
+// var a - dec - undefind
+// var a = 10 - init
+// var a = 20 - re-declare
+// ES5
+// ES6 - ES6+
+
+// let
+// let a;
+// let a = 10;
+// a += 20 a = a+20 
+
+ 
+// const a 
+// const a = 10
+// const a = 11
+//  a = 11
+
+// const a
+
+
+
+
+// const a = 10
+// console.log(a);
+
+// function add(){
+
+//    console.log('running');
+   
+
+// }
+// add()
+// let add = function (){
+//    console.log('running');
+   
+// }
+
+// let add = ()=>{
+
+// }
+
+
+// console.log('start');
+
+
+// for(let a = 1; a <= 1000;a++){
+
+//    console.log(a);
+   
+// }
+
+// console.log('stop');
+
+
+
+// AACLID
+
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+
+
+// let a = 0
+// let b = 34
+
+// let c = a != 10
+
+// if(!a != 10){
+
+//    console.log(a);
+   
+// } 
+
+
+//Primitive Datatype
+
+// const title = ""
+// const age = 20
+// const isNull = null;
+// const isActive = true
+
+
+// console.log(`This is the data types - ${title},${age},${isNull},${isActive}`);
+
+
+// if(title) {
+   
+//    console.log('running');
+   
+// }
+
+
+// non primitive data type
+
+// const arr = [1,2,3,4,"react",5,6,]
+
+// const obj = {name:"react",react:[1,2,3,4]}
+
+
+// const arrobj = [
+//  {name:"react",react:[1,2,3,4]}
+// ,{name:"react",react:[1,2,3,4]}
+// ,{name:"react",react:[1,2,3,4]}
+// ]
+
+
+let single = 0
+
+for(let x=0;x<10;x++){
+
+
+
+    
+
+  for(let y = 0;y<x;y++){
+
+    
+    console.log(x,y)
+    
+    
+  }
+   
+  
+
+
+}
+
+

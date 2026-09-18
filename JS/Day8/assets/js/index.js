c02340243f363741b5b2ad49e7bc1ea697b943b1
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-18 16:29:06 +0530

    Day8 completed

---
+++ JS/Day8/assets/js/index.js
@@ -0,0 +1,38 @@
+// DOM - Browser api
+// Document - HTML
+// Object   - {key:value}
+// Modle    - Stucture 
+
+
+
+// HTML - tags - Elements - block level - inline - semetaic - non sementic - format  interactive - attibute 
+
+// Selector - getElementByID,class,tag,queryselector,queryselectorall
+
+// js - Events - oparet - Event Lisener() OR (inline function trigger)
+
+
+// Properties
+// innerHTML
+// innerText
+// textContent
+// value
+// src
+// style.color 
+// window.location.href
+
+// console.log(document.body.h1);
+
+
+// const title = document.querySelector("#title")
+
+// const btn = document.querySelector("#btn")
+
+// const changeText = ()=>{
+
+//     title.textContent = "This is Js DOM Process"
+//     title.style.color = "yellow"
+
+// }
+
+

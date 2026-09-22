a65094748f304336996817f9c5648577bd9fc0b2
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-22 16:45:20 +0530

    React Class

---
+++ React/Day2/viteproject/src/App.jsx
@@ -0,0 +1,25 @@
+import  { Component } from 'react';
+
+class App extends Component{
+
+ 
+
+  render (){
+    return <>
+     <div>
+      This is the React Class
+     </div>
+    </>
+  }
+
+    
+
+
+}
+
+export default App
+
+
+
+
+
